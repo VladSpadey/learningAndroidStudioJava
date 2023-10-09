73aba159ecb396b05bb89a0af6a511345a429b5d
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-10-09 15:44:53 -0700

    chore: Emptied all the files.

--- app/src/main/java/com/example/android_tutorial1/MainActivity.java
+++ app/src/main/java/com/example/android_tutorial1/MainActivity.java
@@ -12,7 +12,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void onHelloBtnClicked(View view) {
-    }
 }
